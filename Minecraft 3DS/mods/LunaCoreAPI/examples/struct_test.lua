ef6f5708828a60c011c44bf71ff126f51d1bbd8a
Phin Beresford <78656905+Cracko298@users.noreply.github.com>
2026-05-20 11:31:07 -0400

    Delete Minecraft 3DS/mods/LunaCoreAPI directory

--- Minecraft 3DS/mods/LunaCoreAPI/examples/struct_test.lua
+++
@@ -1,7 +0,0 @@
-local stoneTier = CoreAPI.Tools.Tiers.STONE
-stoneTier.Durability = -1
-
-Game.Gamepad.OnKeyPressed:Connect(function ()
-    Core.Debug.message("Stone tier")
-    Core.Debug.message("Durability: "..stoneTier.Durability)
-end)
